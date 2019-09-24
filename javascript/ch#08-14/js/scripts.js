1cda0e280773bebcd8d747f77706eb88446d7c42
umartunes <s.umarahmad@gmail.com>
2019-09-24 23:48:19 +0500

    CSS & Javascript codes added

---
+++ javascript/ch#08-14/js/scripts.js
@@ -0,0 +1,78 @@
+// Ch # 8
+
+var message = "Good day ";
+var name = "Umar";
+var banger = "!";
+
+alert( message + name + "!" );
+
+
+
+// ----------------------------
+// Ch # 9-10
+// name = prompt( "Your name?" );
+
+// alert( message + name + banger );
+
+// if( name == "Umar" ){
+//     alert( "Good day " + name + "!" );
+// }
+
+
+
+
+// ----------------------------
+// Ch # 11
+// var num1 = 15;
+// var num2 = 15;
+
+// if( num1 > num2 ){
+//     alert( "1st number is greater than 2nd" )
+// }
+
+
+
+
+
+// ----------------------------
+// Ch # 12-13
+
+// var age = prompt( "Your age?" );
+// var weight = prompt( "Your Weight?" );
+
+
+// if( age >= 18 && weight <= 70 ){
+
+//     alert( "You're a smart man." );
+
+// }else if ( age >= 18 && weight >= 70 ){
+
+//     alert( "You're a fat guy." );
+
+// }else{
+
+//     alert( "You're a baby" );
+
+// }
+
+
+// ----------------------------
+// Ch # 14
+
+// var age = prompt("Your age?");
+
+// if (age >= 18) {
+
+//     var weight = prompt("Your Weight?");
+
+//     if (weight < 70) {
+//         alert("You're a smart man.");
+//     } else {
+//         alert("You're a fat guy.");
+//     }
+
+// } else {
+
+//     alert("You're a baby");
+
+// }
